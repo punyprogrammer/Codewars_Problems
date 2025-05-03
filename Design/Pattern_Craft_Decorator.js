c576b6a05894402946c3b06d8de9d9840b7d82c4
Amardeep Ganguly <83149058+punyprogrammer@users.noreply.github.com>
2025-05-03 21:04:41 +0530

    Create Pattern_Craft_Decorator.js

---
+++ Design/Pattern_Craft_Decorator.js
@@ -0,0 +1,35 @@
+class Marine {
+  constructor(_damage, _armor) {
+    
+  }
+  
+  get damage() { return 0; }
+  set damage(value) {  }
+  
+  get armor() { return 0; }
+  set armor(value) {  }
+}
+
+class MarineWeaponUpgrade {
+  constructor(marine) {
+    
+  }
+  
+  get damage() { return 0; }
+  set damage(value) {  }
+  
+  get armor() { return 0; }
+  set armor(value) {  }
+}
+
+class MarineArmorUpgrade {
+  constructor(marine) {
+    
+  }
+  
+  get damage() { return 0; }
+  set damage(value) {  }
+  
+  get armor() { return 0; }
+  set armor(value) {  }
+}
